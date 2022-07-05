cc94fd436c6d187d10fcde472c7f31ac76dd7c2b
kolomensky <e.kolomensky@gmail.com>
2022-07-05 14:21:53 +0300

    sum

---
+++ swgate_files/index-0e426b236eba6baa.js
@@ -0,0 +1,126 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+    [405], {
+        5301: function(e, t, s) {
+            (window.__NEXT_P = window.__NEXT_P || []).push(["/", function() {
+                return s(8226)
+            }])
+        },
+        8226: function(e, t, s) {
+            "use strict";
+            s.r(t), s.d(t, {
+                default: function() {
+                    return x
+                }
+            });
+            var A = s(5893),
+                r = s(7533),
+                n = s(5675),
+                a = s(1664),
+                i = s(7294),
+                c = {
+                    src: "./swgate_files/mm(1).jpg",
+                    height: 3e3,
+                    width: 3e3,
+                    blurDataURL: "data:image/jpeg;base64,/9j/2wBDAAoHBwgHBgoICAgLCgoLDhgQDg0NDh0VFhEYIx8lJCIfIiEmKzcvJik0KSEiMEExNDk7Pj4+JS5ESUM8SDc9Pjv/2wBDAQoLCw4NDhwQEBw7KCIoOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozv/wAARCAAIAAgDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAb/xAAfEAABAwMFAAAAAAAAAAAAAAABAAIDBAUREhMUIlH/xAAVAQEBAAAAAAAAAAAAAAAAAAACA//EABgRAAMBAQAAAAAAAAAAAAAAAAABAhEx/9oADAMBAAIRAxEAPwCem5FysETaaqnmfLuMlDndGkY0gEe5REU5lTwVU3mn/9k="
+                },
+                l = s(9858),
+                o = s(3997),
+                h = [{
+                    href: "https://www.instagram.com/telecom.pro/",
+                    text: "instagram"
+                }, {
+                    href: "https://www.linkedin.com/in/kolomensky/",
+                    text: "linkedin"
+                }],
+                f = function() {
+                    var e = (0, i.useState)(!1),
+                        t = e[0],
+                        s = e[1];
+                    return (0, A.jsxs)(A.Fragment, {
+                        children: [(0, A.jsx)(l.K, {
+                            persist: !0,
+                            stateKey: "ok3",
+                            folders: [{
+                                href: ".family.html",
+                                style: l.b.Default,
+                                name: "family",
+                                x: .65,
+                                y: .75                            
+                            }, {
+                                href: "/two",
+                                style: l.b.Black,
+                                name: "work",
+                                x: .55,
+                                y: .15
+                            }, {
+                                href: "/three",
+                                style: l.b.Gray,
+                                name: "hobbies",
+                                x: .18,
+                                y: .48
+                           
+                            }]
+                        
+                        }), (0, A.jsx)("div", {
+                            className: "fixed bottom-8 left-0 right-0 h-0 flex justify-center items-center z-30",
+                            children: (0, A.jsx)("div", {
+                                className: "absolute bottom-0 text-black/50 font-mono",
+                                children: h.map((function(e, t) {
+                                    return (0, A.jsxs)(i.Fragment, {
+                                        children: [0 !== t ? (0, A.jsx)(A.Fragment, {
+                                            children: " / "
+                                        }) : null, (0, A.jsx)("a", {
+                                            className: "hover:text-[#cd3f25]",
+                                            href: e.href,
+                                            target: "_blank",
+                                            rel: "noreferrer",
+                                            children: e.text
+                                        })]
+                                    }, t)
+                                }))
+                            })
+                        }), (0, A.jsx)(r.V, {
+                            open: t,
+                            onClose: function() {
+                                return s(!1)
+                            },
+                            static: !0,
+                            children: (0, A.jsxs)("div", {
+                                className: t ? "block" : "hidden",
+                                children: [(0, A.jsx)("div", {
+                                    className: "fixed inset-0 bg-black/50 z-40",
+                                    "aria-hidden": "true"
+                                }), (0, A.jsx)("div", {
+                                    className: "fixed inset-0 flex items-center justify-center z-40",
+                                    children: (0, A.jsx)(r.V.Panel, {
+                                        className: "mx-auto bg-white max-h-[calc(min(100vh,100vw,800px)-32px)] max-w-[calc(min(100vh,100vw,800px)-32px)]",
+                                        children: (0, A.jsx)(n.default, {
+                                            src: c,
+                                            priority: !0
+                                        })
+                                    })
+                                })]
+                            })
+                        })]
+                    })
+                };
+            f.getLayout = function(e) {
+                var t = e.children;
+                return (0, A.jsxs)(A.Fragment, {
+                    children: [(0, A.jsx)(o.y, {
+                        title: "swgate"
+                    }), t]
+                })
+            };
+            var x = f
+        }
+    },
+    function(e) {
+        e.O(0, [675, 113, 787, 774, 888, 179], (function() {
+            return t = 5301, e(e.s = t);
+            var t
+        }));
+        var t = e.O();
+        _N_E = t
+    }
+]);
